d15cdb0a3bdd4ba67c744a8c7efe75ac0f9b8507
Will Schulz <will.schulz29@gmail.com>
2019-04-12 11:48:47 -0400

    MVP complete

--- api/server.js
+++ api/server.js
@@ -12,4 +12,8 @@ server.use(express.json());
 
 configureRoutes(server);
 
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
+
 module.exports = server;
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
+
+const secret = require('../config/secrets.js');
+const Users = require('../users/users-model.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +15,38 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+  .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
@@ -29,4 +62,16 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // subject in payload is what the token is about
+    username: user.username
+    // ... other options
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secret.jwtSecret, options)
 }
